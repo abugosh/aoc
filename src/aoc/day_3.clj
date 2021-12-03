4b111b6a63fba2525d373250fa0af2c514ace06b
Alex Bugosh <abugosh@gmail.com>
2021-12-03 09:33:42 -0500

    Day 3 complete!

--- src/aoc/day_3.clj
+++ src/aoc/day_3.clj
@@ -3,19 +3,46 @@
             [clojure.core.match :refer [match]]])
 
 (def data 
-  (map (fn [x]
-         x)
-       (s/split (slurp "resources/day-3.txt") #"\n")))
+  (->> (s/split (slurp "resources/day-3.txt") #"\n")
+       (map (fn [x]
+              (->> (s/split x #"")
+                   (map #(Integer/parseInt %1)))))))
+
+(defn bin->int
+  [bin]
+  (Integer/parseInt (apply str bin) 2))
+
+(defn freqs
+  [input]
+  (reduce (fn [acc x] 
+            (map + acc x))
+          input))
+
+(defn common
+  [f xs]
+  (let [half (/ (count xs) 2)]
+    (map #(if (f half %1) "1" "0") (freqs xs))))
 
 (defn part-one
   ([] (part-one data))
   ([input]
-   input))
+   (* (bin->int (common > input))
+      (bin->int (common < input)))))
+
+(defn find-root
+  [f xs]
+  (let [cur (Integer/parseInt (first (common f xs)))
+        poss (filter #(= (first %1) cur) xs)]
+    (cond
+      (empty? poss) nil
+      (= 1 (count poss)) (first poss)
+      :else (cons cur (find-root f (map rest poss))))))
 
 (defn part-two
   ([] (part-two data))
   ([input]
-   input))
+   (* (bin->int (find-root > input))
+      (bin->int (find-root <= input)))))
 
 (part-one)
 (part-two)
