e021cbd3de580e02ecb0e988a49a23b480391982
Alex Bugosh <abugosh@gmail.com>
2021-12-07 08:13:48 -0500

    Done Day 7!

--- src/aoc/core.clj
+++ src/aoc/core.clj
@@ -5,7 +5,8 @@
             [aoc.day-3]
             [aoc.day-4]
             [aoc.day-5]
-            [aoc.day-6]])
+            [aoc.day-6]
+            [aoc.day-7]])
 
 (defn -main
   "Print all of the answers"
---
+++ src/aoc/day_7.clj
@@ -0,0 +1,44 @@
+(ns aoc.day-7
+  [:require 
+   [aoc.utils :as u]
+   [clojure.string :as s]
+   [clojure.core.match :refer [match]]])
+
+(def data
+  (map u/parse-int (s/split (s/trim (slurp "resources/day-7.txt")) #",")))
+
+(defn flat-cost
+  [start end]
+  (Math/abs (- start end)))
+
+(defn cost->list
+  [point f xs]
+  (->> xs
+       (map (partial f point))
+       (apply +)))
+
+(defn find-cost
+  [f crabs]
+  (->> (range 0 (last (sort crabs)))
+        (map #(cost->list %1 f crabs))
+        sort
+        first))
+
+(defn part-one
+  ([] (part-one data))
+  ([input]
+   (find-cost flat-cost input)))
+
+(def exp-cost-memo (memoize (fn [x] (apply + (range 1  (inc x))))))
+
+(defn exp-cost
+  [start end] 
+  (exp-cost-memo (flat-cost start end)))
+
+(defn part-two
+  ([] (part-two data))
+  ([input]
+   (find-cost exp-cost input)))
+
+(part-one)
+(part-two)
