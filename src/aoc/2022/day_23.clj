3f776efc6a6ae88a4b4733b829ca30fa7681781d
Alex Bugosh <abugosh@gmail.com>
2022-11-29 01:46:52 -0500

    Prepopulating the files for 2022

---
+++ src/aoc/2022/day_23.clj
@@ -0,0 +1,17 @@
+(ns aoc.2020.day-23
+  [:require
+   [aoc.utils :as u]
+   [clojure.string :as s]])
+
+(def data
+  (->> (slurp "resources/2022/day-23.txt")))
+
+(defn part-one
+  ([] (part-one data))
+  ([input]
+   nil))
+
+(defn part-two
+  ([] (part-two data))
+  ([input]
+   nil))
