dd35f356a0c7e36dafe662672be06b93fe27e790
Alex Bugosh <abugosh@gmail.com>
2022-12-02 08:24:39 -0500

    Complete day 3 2022

--- src/aoc/2022/day_3.clj
+++ src/aoc/2022/day_3.clj
@@ -1,17 +1,37 @@
 (ns aoc.2020.day-3
   [:require
    [aoc.utils :as u]
+   [clojure.set :as st]
    [clojure.string :as s]])
 
 (def data
-  (->> (slurp "resources/2022/day-3.txt")))
+  (->> (slurp "resources/2022/day-3.txt")
+       s/split-lines
+       (map (fn [line]
+              [(apply hash-set (map int (subs line 0 (/ (count line) 2))))
+               (apply hash-set (map int (subs line (/ (count line) 2))))]))))
+
+(defn priority [c]
+  (if (>= c (int \a))
+    (inc (- c (int \a)))
+    (+ 27 (- c (int \A)))))
 
 (defn part-one
   ([] (part-one data))
   ([input]
-   nil))
+   (->> input
+        (map #(apply st/intersection %))
+        (map first)
+        (map priority)
+        (apply +))))
 
 (defn part-two
   ([] (part-two data))
   ([input]
-   nil))
+   (->> input
+        (map #(apply st/union %))
+        (partition 3)
+        (map #(apply st/intersection %))
+        (map first)
+        (map priority)
+        (apply +))))
