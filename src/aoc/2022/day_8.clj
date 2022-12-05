53f8f9f4686a26e0fe9fc7acdc4961f61a1795bc
Alex Bugosh <abugosh@gmail.com>
2022-12-05 16:57:43 -0500

    Completed day 8 2022

--- src/aoc/2022/day_8.clj
+++ src/aoc/2022/day_8.clj
@@ -4,14 +4,61 @@
    [clojure.string :as s]])
 
 (def data
-  (->> (slurp "resources/2022/day-8.txt")))
+  (->> (slurp "resources/2022/day-8.txt")
+       s/split-lines
+       (map #(s/split % #""))
+       (mapv (partial mapv u/parse-int))))
+
+(defn visible-from? [map [yp xp] [y x]]
+  (let [hieght (get-in map [y x])]
+    (loop [y (+ y yp)
+           x (+ x xp)]
+      (let [loc (get-in map [y x] :vis)]
+        (cond
+          (= loc :vis) true
+          (>= loc hieght) false
+          :else (recur (+ y yp) (+ x xp)))))))
+
+(defn visible-trees [map]
+  (for [y (range (count map))
+        x (range (count (first map)))
+        :when (or (visible-from? map [1 0] [y x])
+                  (visible-from? map [0 1] [y x])
+                  (visible-from? map [-1 0] [y x])
+                  (visible-from? map [0 -1] [y x]))]
+    [y x]))
 
 (defn part-one
   ([] (part-one data))
   ([input]
-   nil))
+   (->> input
+        visible-trees
+        count)))
+
+(defn trees-in-view [map [yp xp] [y x]]
+  (let [hieght (get-in map [y x])]
+    (loop [y (+ y yp)
+           x (+ x xp)
+           trees 0]
+      (let [loc (get-in map [y x] :vis)]
+        (cond
+          (= loc :vis) trees
+          (>= loc hieght) (inc trees)
+          :else (recur (+ y yp) (+ x xp) (inc trees)))))))
+
+(defn score-trees [map]
+  (for [y (range (count map))
+        x (range (count (first map)))
+        :let [score (* (trees-in-view map [1 0] [y x])
+                       (trees-in-view map [0 1] [y x])
+                       (trees-in-view map [-1 0] [y x])
+                       (trees-in-view map [0 -1] [y x]))]]
+    score))
 
 (defn part-two
   ([] (part-two data))
   ([input]
-   nil))
+   (->> input
+        score-trees
+        flatten
+        (apply max))))
