c8aca7a92840de598fb600c4efdb9c47d1e9f20a
Alex Bugosh <abugosh@gmail.com>
2022-12-11 21:58:25 -0500

    Completed day 13 2022

--- src/aoc/2022/day_13.clj
+++ src/aoc/2022/day_13.clj
@@ -1,17 +1,56 @@
 (ns aoc.2020.day-13
   [:require
-   [aoc.utils :as u]
+   [clojure.core.match :refer [match]]
    [clojure.string :as s]])
 
 (def data
-  (->> (slurp "resources/2022/day-13.txt")))
+  (->> (slurp "resources/2022/day-13.txt")
+       s/split-lines
+       (remove empty?)
+       (map read-string)))
+
+(defn packet-ordered? [[left & lr] [right & rr]]
+  (match [left right]
+         [nil nil] nil
+         [nil right] true
+         [left nil] false
+         [(left :guard vector?) (right :guard int?)] (packet-ordered? left [right])
+         [(left :guard int?) (right :guard vector?)] (packet-ordered? [left] right)
+         [(left :guard vector?) (right :guard vector?)] (let [order (packet-ordered? left right)]
+                                                          (if (nil? order)
+                                                            (packet-ordered? lr rr)
+                                                            order))
+         [(left :guard int?) (right :guard int?)] (cond
+                                                    (< left right) true
+                                                    (> left right) false
+                                                    :else (packet-ordered? lr rr))))
 
 (defn part-one
   ([] (part-one data))
   ([input]
-   nil))
+   (->> input
+        (partition 2)
+        (map (partial apply packet-ordered?))
+        (map-indexed #(and %2 (inc %1)))
+        (filter int?)
+        (apply +))))
+
+(defn packet-comp [p1 p2]
+  (if (packet-ordered? p1 p2)
+    -1
+    1))
+
+(defn packet-index [packets p]
+  (->> packets
+       (split-with (partial not= p))
+       first
+       count
+       inc))
 
 (defn part-two
   ([] (part-two data))
   ([input]
-   nil))
+   (let [packets (->> input
+                      (concat (list [[2]] [[6]]))
+                      (sort-by identity packet-comp))]
+     (* (packet-index packets [[2]]) (packet-index packets [[6]])))))
