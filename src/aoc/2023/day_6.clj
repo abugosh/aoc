8a6306430b0b1df25349fce6771d712ac57742aa
Alex Bugosh <abugosh@gmail.com>
2023-12-06 09:24:09 -0500

    Complete data 6 2023!

--- src/aoc/2023/day_6.clj
+++ src/aoc/2023/day_6.clj
@@ -3,14 +3,34 @@
    [clojure.string :as s]])
 
 (def data
-  (->> (slurp (format "resources/2023/%s.txt" (last (s/split (str (ns-name *ns*)) #"\."))))))
+  (->> (slurp (format "resources/2023/%s.txt" (last (s/split (str (ns-name *ns*)) #"\."))))
+       s/split-lines
+       (map #(s/split % #":"))
+       (map last)
+       (map s/trim)
+       (map #(s/split % #"\W+"))))
+
+(defn find-record-breakers [[time record]]
+  (for [t (range 0 time)
+        :let [dist (* (- time t) t)]
+        :when (> dist record)]
+    t))
 
 (defn part-one
   ([] (part-one data))
   ([input]
-   nil))
+   (->> input
+        (map (partial map parse-long))
+        (apply map vector)
+        (map find-record-breakers)
+        (map count)
+        (apply *))))
 
 (defn part-two
   ([] (part-two data))
   ([input]
-   nil))
+   (->> input
+        (map (partial apply str))
+        (map parse-long)
+        find-record-breakers
+        count)))
