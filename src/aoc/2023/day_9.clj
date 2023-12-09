188ec4d11414d116d62099bdbeecdf6430787a85
Alex Bugosh <abugosh@gmail.com>
2023-12-09 07:22:24 -0500

    Complete day 9 2023!

--- src/aoc/2023/day_9.clj
+++ src/aoc/2023/day_9.clj
@@ -3,14 +3,33 @@
    [clojure.string :as s]])
 
 (def data
-  (->> (slurp (format "resources/2023/%s.txt" (last (s/split (str (ns-name *ns*)) #"\."))))))
+  (->> (slurp (format "resources/2023/%s.txt" (last (s/split (str (ns-name *ns*)) #"\."))))
+       s/split-lines
+       (map #(s/split % #" "))
+       (map (partial map parse-long))))
+
+(defn build-differences [base]
+  (loop [cur base
+         diffs []]
+    (if (every? zero? cur)
+      diffs
+      (recur (map - (drop 1 cur) cur) (conj diffs cur)))))
 
 (defn part-one
   ([] (part-one data))
   ([input]
-   nil))
+   (->> input
+        (map build-differences)
+        (map (partial map last))
+        (map (partial apply +))
+        (apply +))))
 
 (defn part-two
   ([] (part-two data))
   ([input]
-   nil))
+   (->> input
+        (map build-differences)
+        (map (partial map first))
+        (map reverse)
+        (map (partial reduce #(- %2 %1)))
+        (apply +))))
