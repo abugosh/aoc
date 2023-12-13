67593ed2569d255abbfc8899f629bb7e6e9eed04
Alex Bugosh <abugosh@gmail.com>
2023-12-13 08:39:14 -0500

    Complete day 13 2023!

--- src/aoc/2023/day_13.clj
+++ src/aoc/2023/day_13.clj
@@ -3,14 +3,50 @@
    [clojure.string :as s]])
 
 (def data
-  (->> (slurp (format "resources/2023/%s.txt" (last (s/split (str (ns-name *ns*)) #"\."))))))
+  (->> (slurp (format "resources/2023/%s.txt" (last (s/split (str (ns-name *ns*)) #"\."))))
+       s/split-lines
+       (partition-by (partial = ""))
+       (remove #(empty? (first %)))
+       (map (fn [x] (->> x
+                         (map #(s/split % #""))
+                         (mapv (partial mapv keyword)))))))
+
+(defn pivot [board]
+  (apply mapv vector board))
+
+(defn mirror-index? [board i]
+  (let [[a b] (split-at i board)]
+    (->> (map = (reverse a) b)
+         (every? identity))))
+
+(defn find-mirror-index [test board]
+  (->> board
+       (map vector (drop 1 board))
+       (keep-indexed (fn [i [a b :as c]] (if (or (= a b) (test c 1)) (inc i))))
+       (some #(and (test board %) %))))
+
+(defn score [test board]
+  (let [h-index (or (find-mirror-index test board) 0)
+        v-index (or (find-mirror-index test (pivot board)) 0)]
+    (+ v-index (* 100 h-index))))
 
 (defn part-one
   ([] (part-one data))
   ([input]
-   nil))
+   (->> input
+        (map (partial score mirror-index?))
+        (apply +))))
+
+(defn smudge-index? [board i]
+  (let [[a b] (split-at i board)
+        lines (->> (map (juxt = vector) (reverse a) b) (remove first))]
+    (if (= 1 (count lines))
+      (->> lines first second (apply map =) (remove identity) count (= 1))
+      false)))
 
 (defn part-two
   ([] (part-two data))
   ([input]
-   nil))
+   (->> input
+        (map (partial score smudge-index?))
+        (apply +))))
