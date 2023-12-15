a78498b77f5933e6a4b801be904871ee415a0e4c
Alex Bugosh <abugosh@gmail.com>
2023-12-15 07:42:17 -0500

    Complete day 15 2023!

--- src/aoc/2023/day_15.clj
+++ src/aoc/2023/day_15.clj
@@ -3,14 +3,46 @@
    [clojure.string :as s]])
 
 (def data
-  (->> (slurp (format "resources/2023/%s.txt" (last (s/split (str (ns-name *ns*)) #"\."))))))
+  (->> (s/split (s/trim (slurp (format "resources/2023/%s.txt" (last (s/split (str (ns-name *ns*)) #"\."))))) #",")))
+
+(defn init-hash [v]
+  (->> v
+       seq
+       (map int)
+       (reduce (fn [acc x] (rem (* 17 (+ acc x)) 256)) 0)))
 
 (defn part-one
   ([] (part-one data))
   ([input]
-   nil))
+   (->> input
+        (map init-hash)
+        (apply +))))
+
+(defn parse-ins [ins]
+  (if (s/ends-with? ins "-")
+    (let [label (s/replace ins #"-" "")]
+      {:op :- :label label :loc (init-hash label)})
+    (let [[label loc] (s/split ins #"=")]
+      {:op := :label label :fl (parse-long loc) :loc (init-hash label)})))
+
+(defn run-seq [ins-seq]
+  (loop [[{:keys [op label loc fl] :as ins} & ins-seq] ins-seq
+         boxen {}]
+    (let [box (get boxen loc [])]
+      (cond
+        (nil? ins) boxen
+        (= op :-) (recur ins-seq (assoc boxen loc (->> box (remove #(= label (:label %))))))
+        (= op :=) (recur ins-seq (assoc boxen loc (let [[a b] (split-with #(not= label (:label %)) box)]
+                                                    (concat a [ins] (drop 1 b)))))))))
+
+(defn calc-focus-power [boxen]
+  (reduce (fn [acc [k v]] (apply + acc (map-indexed (fn [i {fl :fl}] (* (inc k) (inc i) fl)) v)))
+          0 boxen))
 
 (defn part-two
   ([] (part-two data))
   ([input]
-   nil))
+   (->> input
+        (map parse-ins)
+        run-seq
+        calc-focus-power)))
