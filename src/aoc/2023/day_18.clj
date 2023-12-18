194e9e7bce4076040de40dc55066a9fcf4c8201d
Alex Bugosh <abugosh@gmail.com>
2023-12-18 11:33:20 -0500

    Complete day 18 2023!

--- src/aoc/2023/day_18.clj
+++ src/aoc/2023/day_18.clj
@@ -2,15 +2,94 @@
   [:require
    [clojure.string :as s]])
 
+(defn parse-line [line]
+  (let [[_ dir num color] (re-matches #"([UDLR]) (\d+) \(#(\w+)\)" line)]
+    [(keyword dir) (parse-long num) color]))
+
 (def data
-  (->> (slurp (format "resources/2023/%s.txt" (last (s/split (str (ns-name *ns*)) #"\."))))))
+  (->> (slurp (format "resources/2023/%s.txt" (last (s/split (str (ns-name *ns*)) #"\."))))
+       s/split-lines
+       (map parse-line)))
+
+(def dir-map {:U [-1 0]
+              :D [1 0]
+              :R [0 1]
+              :L [0 -1]})
+
+(defn gen-pnt [dir pnt]
+  (mapv + pnt (dir-map dir)))
+
+(defn dig [start [dir len]]
+  (->> start
+       (iterate (partial gen-pnt dir))
+       (drop 1)
+       (take len)))
+
+(defn follow-ins [ins-list]
+  (loop [pnt [0 0]
+         [ins & ins-list] ins-list
+         trench #{}
+         colors {}]
+    (if (nil? ins)
+      {:lagoon trench :colors colors}
+      (let [seg (dig pnt ins)]
+        (recur (last seg) ins-list (into trench seg) (into colors (map vector seg (repeat (last ins)))))))))
+
+(defn flood-dig [trench]
+  (loop [[cur & open] [[1 1]]
+         lagoon trench]
+    (cond
+      (nil? cur) lagoon
+      (lagoon cur) (recur open lagoon)
+      :else (recur (concat open (map #(gen-pnt % cur) [:U :D :R :L])) (conj lagoon cur)))))
 
 (defn part-one
   ([] (part-one data))
   ([input]
-   nil))
+   (->> input
+        follow-ins
+        :lagoon
+        flood-dig
+        count)))
+
+(defn parse-color [color]
+  (let [len (Integer/parseInt (s/join (drop-last 1 color)) 16)
+        dir ([:R :D :L :U] (parse-long (str (last color))))]
+    [dir len]))
+
+(defn parse-color-ins [ins-list]
+  (->> ins-list
+       (map last)
+       (map parse-color)))
+
+(defn ins-segments [ins-list]
+  (loop [cur [0 0]
+         [[dir len :as ins] & ins-list] ins-list
+         segments []]
+    (if (nil? ins)
+      segments
+      (let [end (mapv + cur (map (partial * len) (dir-map dir)))]
+        (recur end ins-list (conj segments [cur end]))))))
+
+(defn normalize-segments [segments]
+  (let [x-offset (->> segments (apply concat) (map first) (apply min) abs)
+        y-offset (->> segments (apply concat) (map second) (apply min) abs)]
+    (->> segments
+         (map (fn [[[x1 y1] [x2 y2]]] [[(+ x1 x-offset) (+ y1 y-offset)] [(+ x2 x-offset) (+ y2 y-offset)]])))))
+
+(defn shoelace [segments]
+  (/ (->> segments
+          (map (fn [[[x1 y1] [x2 y2]]] (- (* x2 y1) (* x1 y2))))
+          (apply +))
+     2))
 
 (defn part-two
   ([] (part-two data))
   ([input]
-   nil))
+   (let [ins-list (parse-color-ins input)
+         perimeter (inc (/ (apply + (map second ins-list)) 2))]
+     (->> ins-list
+          ins-segments
+          normalize-segments
+          shoelace
+          (+ perimeter)))))
